5b2bf288993b5259b1b4fc55a32ae0d2b2ffa0be
Hala Alotaibi <152783900+halaturki-alotaibi@users.noreply.github.com>
2025-10-19 14:01:34 +0300

    Delete SQL/customer_segment.sql

--- SQL/customer_segment.sql
+++
@@ -1,9 +0,0 @@
-SELECT *,
-  CASE
-    WHEN total_revenue >= 5000 THEN 'Platinum'
-    WHEN total_revenue BETWEEN 2000 AND 4999 THEN 'Gold'
-    WHEN total_revenue BETWEEN 1000 AND 1999 THEN 'Silver'
-    WHEN total_revenue BETWEEN 500 AND 999 THEN 'Bronze'
-    ELSE 'Basic'
-  END AS customer_segment
-FROM `saas-data-474609.1.customer_summary`;
